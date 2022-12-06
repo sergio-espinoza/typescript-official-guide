fbec7cfcc1c1ce1c804262b4e23af1c785157f48
Sergio Junior Espinoza Peralta <48479567.sjep@gmail.com>
2022-12-05 20:46:45 -0500

    the hardest task so far resolved

---
+++ public/hoverIntent.js
@@ -0,0 +1,82 @@
+'use strict';
+
+// source: https://javascript.info/mousemove-mouseover-mouseout-mouseenter-mouseleave#smart-tooltip
+
+// Here's a brief sketch of the class
+// with things that you'll need anyway
+class HoverIntent {
+  isOver = false;
+  currentX = 0;
+  currentY = 0;
+  currentTimeStamp = 1;
+  timeout = -1;
+
+  constructor({
+    sensitivity = 0.1, // speed less than 0.1px/ms means "hovering over an element"
+    interval = 100, // measure mouse speed once per 100ms: calculate the distance between previous and next points
+    elem,
+    over,
+    out,
+  }) {
+    this.sensitivity = sensitivity;
+    this.interval = interval;
+    this.elem = elem;
+    this.over = over;
+    this.out = out;
+
+    // make sure "this" is the object in event handlers.
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseOver = this.onMouseOver.bind(this);
+    this.onMouseOut = this.onMouseOut.bind(this);
+
+    // assign the handlers
+    elem.addEventListener('mouseover', this.onMouseOver);
+    elem.addEventListener('mouseout', this.onMouseOut);
+    elem.addEventListener('mousemove', this.onMouseMove);
+
+    // continue from this point
+  }
+
+  onMouseOver(event) {
+    /* ... */
+    this.isOver = true;
+    this.timeout = setTimeout(() => this.isOver && this.over(), this.interval);
+  }
+
+  onMouseOut(event) {
+    /* ... */
+    this.isOver = this.elem?.contains(event.relatedTarget);
+
+    if (this.isOver) {
+      return;
+    }
+
+    clearTimeout(this.timeout);
+    this.out();
+  }
+
+  onMouseMove(event) {
+    const { x = 0, y = 0, timeStamp = 1 } = event || {};
+
+    this.differenceX = x - this.currentX;
+    this.differenceY = y - this.currentX;
+    this.differenceTimeStamp = timeStamp - this.currentTimeStamp;
+
+    const hypotenuse = Math.sqrt(this.differenceX ** 2 + this.differenceY ** 2);
+    this.isOver = hypotenuse / this.differenceTimeStamp < this.sensitivity;
+
+    this.currentNow = timeStamp;
+    this.currentX = x;
+    this.currentY = y;
+
+    /* ... */
+  }
+
+  destroy() {
+    /* your code to "disable" the functionality, remove all handlers */
+    /* it's needed for the tests to work */
+    this.elem.removeEventListener('mouseover', this.onMouseOver);
+    this.elem.removeEventListener('mouseout', this.onMouseOut);
+    this.elem.removeEventListener('mousemove', this.onMouseMove);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,3 @@
 import './style.css';
 
-// source: https://javascript.info/mousemove-mouseover-mouseout-mouseenter-mouseleave#smart-tooltip
-
-import { HoverIntent } from './hoverIntent';
-
-
-export const elemEl = (globalThis as any).elem;
-export const tooltipEl = (globalThis as any).tooltip;
-
-tooltipEl && (tooltipEl.className = 'tooltip');
-tooltipEl && (tooltipEl.innerHTML = 'Tooltip');
-
-new HoverIntent({
-  elem: elemEl,
-  over() {
-    if (!tooltipEl) {
-      return;
-    }
-    
-    const { left, bottom } = elemEl?.getBoundingClientRect() || {
-      left: 0,
-      bottom: 0,
-    };
-    
-    tooltipEl.style.left = left + 5 + 'px';
-    tooltipEl.style.bottom = bottom + 5 + 'px';
-  },
-  out() {
-    tooltipEl?.remove();
-  },
-});
-
-
 
